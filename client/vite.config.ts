474198061c02185fca070ae63bc49e44e0a24b98
Viktor <viktor+git@neuronhub.app>
2025-04-22 21:27:29 -0700

    build: upgrade vite-tsconfig-paths from 5 to 6 (no breaking changes)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,38 +1,11 @@
-import * as path from "node:path";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   root: __dirname,
-
   server: {
     port: 3000,
   },
   plugins: [reactRouter(), tsconfigPaths()],
-
-  resolve: process.env.USE_SOURCE
-    ? {
-        alias: {
-          ...getAliasReactRouterGenerated(),
-          ...getAliasForProject(),
-        },
-      }
-    : {
-        alias: getAliasForProject(),
-      },
 });
-
-function getAliasForProject() {
-  return {
-    "~/graphql": path.resolve(__dirname, "./graphql"),
-    "@": path.resolve(__dirname, "./src"),
-  };
-}
-
-function getAliasReactRouterGenerated() {
-  return {
-    "react-router": path.resolve(__dirname, "../../packages/react-router/index.ts"),
-    "react-router-dom": path.resolve(__dirname, "../../packages/react-router-dom/index.tsx"),
-  };
-}
