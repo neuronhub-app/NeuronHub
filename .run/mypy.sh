db02a467c34980fcd00426b45f6b6f44bc184dbf
Viktor <viktor+git@neuronhub.app>
2025-07-19 00:58:32 +0000

    refac: drop files replaced by mise.toml

--- .run/mypy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd server || exit
-uv run mypy --show-column-numbers --show-absolute-path --implicit-optional --exclude="migrations/" --config-file=pyproject.toml neuronhub
-cd ..
