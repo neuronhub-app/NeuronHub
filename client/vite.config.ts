ec9b9a0a8f15febdcaa0b85333b8e7f1bff6eaae
Viktor <viktor+git@neuronhub.app>
2025-08-19 18:52:54 -0700

    build: enable Vite sourcemaps in dev & build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,4 +10,6 @@ export default defineConfig({
     allowedHosts: ["localhost", "client"],
   },
   plugins: [reactRouter(), tsconfigPaths(), devtoolsJson()],
+  build: { sourcemap: true },
+  dev: { sourcemap: true },
 });
