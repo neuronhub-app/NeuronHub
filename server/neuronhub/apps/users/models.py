ebc0dcdd197e5e146f2cca24b27f03f5074cd059
Viktor <viktor+git@neuronhub.app>
2025-08-27 15:54:33 -0700

    docs(auth): doc refactor plans on UserCircles

--- server/neuronhub/apps/users/models.py
+++ server/neuronhub/apps/users/models.py
@@ -9,7 +9,9 @@ from neuronhub.apps.orgs.models import Org
 
 
 class User(AbstractUser):
-    #: non-public for privacy. Only username may be shown to User's owner or others
+    #: (atm not implemented)
+    #: Non-public for privacy, only admins can see it. May be used in anonymization or as a `hash(id_uuid, secret)` in user exports.
+    #: End users may only see `username`s through API, not `.id` or `.id_uuid`.
     id_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     owner = models.ForeignKey(
@@ -46,10 +48,11 @@ class User(AbstractUser):
         return str(self.username)
 
 
+# todo refac: rename to UserCircle, and move out `connections` as a M2M field on User, then drop `NAME_DEFAULT`.
 class UserConnectionGroup(models.Model):
     name = models.CharField(max_length=255, blank=True)
 
-    # todo refac: rename to author
+    # todo refac: rename to author (or owner?)
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
