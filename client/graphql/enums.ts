62f5162f5391c139ea83bcc10d3dff2780aa5760
Viktor <viktor+git@neuronhub.app>
2025-08-26 22:06:16 -0700

    fix(API): remove PostTypeInput.seen_by_users
    it isn't user-editable

--- client/graphql/enums.ts
+++ client/graphql/enums.ts
@@ -1,39 +1,39 @@
 export enum PostTypeEnum {
-  Comment = "Comment",
-  Post = "Post",
-  Review = "Review",
-  Tool = "Tool",
+  Comment = 'Comment',
+  Post = 'Post',
+  Review = 'Review',
+  Tool = 'Tool'
 }
 
 export enum ToolType {
-  App = "App",
-  Material = "Material",
-  Other = "Other",
-  Product = "Product",
-  Program = "Program",
-  SaaS = "SaaS",
+  App = 'App',
+  Material = 'Material',
+  Other = 'Other',
+  Product = 'Product',
+  Program = 'Program',
+  SaaS = 'SaaS'
 }
 
 export enum UsageStatus {
-  Interested = "INTERESTED",
-  NotInterested = "NOT_INTERESTED",
-  Used = "USED",
-  Using = "USING",
-  WantToUse = "WANT_TO_USE",
+  Interested = 'INTERESTED',
+  NotInterested = 'NOT_INTERESTED',
+  Used = 'USED',
+  Using = 'USING',
+  WantToUse = 'WANT_TO_USE'
 }
 
 export enum UserListName {
-  Library = "library",
-  ReadLater = "read_later",
+  Library = 'library',
+  ReadLater = 'read_later'
 }
 
 export enum Visibility {
-  Connections = "CONNECTIONS",
-  ConnectionGroupsSelected = "CONNECTION_GROUPS_SELECTED",
-  Internal = "INTERNAL",
-  Private = "PRIVATE",
-  Public = "PUBLIC",
-  Subscribers = "SUBSCRIBERS",
-  SubscribersPaid = "SUBSCRIBERS_PAID",
-  UsersSelected = "USERS_SELECTED",
+  Connections = 'CONNECTIONS',
+  ConnectionGroupsSelected = 'CONNECTION_GROUPS_SELECTED',
+  Internal = 'INTERNAL',
+  Private = 'PRIVATE',
+  Public = 'PUBLIC',
+  Subscribers = 'SUBSCRIBERS',
+  SubscribersPaid = 'SUBSCRIBERS_PAID',
+  UsersSelected = 'USERS_SELECTED'
 }
