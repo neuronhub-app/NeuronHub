05d76dff3525bbcb6f01b5ade7fc9061ef95fc5c
Viktor <viktor+git@neuronhub.app>
2026-01-22 15:45:22 -0800

    refac(perf): add React Compiler to vite.config.ts
    Refs: #111

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { sentryReactRouter } from "@sentry/react-router";
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -14,6 +15,14 @@ export default defineConfig(config => ({
   dev: { sourcemap: true },
   plugins: [
     reactRouter(),
+    // from react.dev docs
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
     tsconfigPaths(),
     devtoolsJson(),
     sentryReactRouter(
