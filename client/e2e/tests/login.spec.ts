e2b19cbf30ad9212a6bdb801aeb17435b93872ca
Viktor <viktor+git@neuronhub.app>
2026-03-02 00:48:34 -0800

    test(E2E): drop logn.spec.ts "invalid credentials" test

--- client/e2e/tests/login.spec.ts
+++ client/e2e/tests/login.spec.ts
@@ -5,23 +5,12 @@ import { PlaywrightHelper } from "@/e2e/helpers/PlaywrightHelper";
 import { ids } from "@/e2e/ids";
 import { urls } from "@/urls";
 
-// will stay unused for a while, mb until v0.3
 test.describe("Login", () => {
   let play: PlaywrightHelper;
 
   test.beforeEach(async ({ page }) => {
     play = new PlaywrightHelper(page);
     await play.dbStubsRepopulate({ is_import_HN_post: false, is_create_single_review: false });
-  });
-
-  test("with invalid credentials", async ({ page }) => {
-    await page.goto(urls.login);
-
-    await play.fill(ids.auth.login.username, "wrong");
-    await play.fill(ids.auth.login.password, "wrong");
-    await play.click(ids.auth.login.submit);
-
-    await expect(play.get(ids.auth.login.error)).toBeVisible();
   });
 
   test("with email", async ({ page }) => {
