0b2d4349d02d4d5ea7f885b94d5dd7b44c2cbce3
Viktor <viktor+git@neuronhub.app>
2026-03-01 18:46:39 -0800

    build(Mise): add flag docker:tag-and-push --pre_release #draft
    The code is bad code, it should:
    - be either all in Python or in Mise/Nu
    - use consistent $env refs
    - restore --tag_only

--- devops/docker_tag_and_push.py
+++ devops/docker_tag_and_push.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 # !# /// script
-# requires-python = ">=3.11"
+# requires-python = ">=3.14"
 # ///
 
 """
-For example output eg run `mise docker:push --tag_only --app=client`
-
-FYI: I'm not often testing py3.11, mostly 3.14.
+See [[docker.mise.toml]]
 """
 
 import argparse
-import os
 import subprocess
 from argparse import Namespace
 from typing import Literal
@@ -20,6 +17,7 @@ parser.add_argument("--app", default="server", choices=["server", "client", "cod
 parser.add_argument("--github_path")
 parser.add_argument("--version")
 parser.add_argument("--tag_only", action="store_true")
+parser.add_argument("--pre_release", action="store_true")
 
 
 class NamespaceKwargs(Namespace):
@@ -27,34 +25,36 @@ class NamespaceKwargs(Namespace):
     version: str
     github_path: str
     tag_only: bool
+    pre_release: bool
+    no_sudo: bool
 
 
 def main(kwargs: NamespaceKwargs):
     version_splits = kwargs.version.split(".")
 
-    for tag_version in [
-        kwargs.version,
-        f"{version_splits[0]}.{version_splits[1]}",  # eg "0.2"
-        f"{version_splits[0]}.{version_splits[1]}.{version_splits[2]}",
-        "latest",
-    ]:
+    if kwargs.pre_release:
+        tag_versions = [kwargs.version]
+    else:
+        tag_versions = [
+            kwargs.version,
+            f"{version_splits[0]}.{version_splits[1]}",  # eg "0.2"
+            f"{version_splits[0]}.{version_splits[1]}.{version_splits[2]}",
+            "latest",
+        ]
+
+    for tag_version in tag_versions:
         container_path = f"ghcr.io/{kwargs.github_path}/{kwargs.app}"
         tag_existing = f"{container_path}:{kwargs.version}"
-        _docker_run("tag", tag_existing, f"{container_path}:{tag_version}")
-
+        subprocess.run(
+            ["docker", "tag", tag_existing, f"{container_path}:{tag_version}"],
+            check=True,
+        )
         is_push_to_registry = not kwargs.tag_only
         if is_push_to_registry:
-            _docker_run("push", f"{container_path}:{tag_version}")
-
-
-def _docker_run(*args: str):
-    if os.environ.get("CI") == "true":
-        docker_bin = ["docker"]
-    else:
-        # local sudo
-        docker_bin = ["sudo", "-E", "docker"]
-    print("docker", *args)
-    subprocess.run([*docker_bin, *args], check=True)
+            subprocess.run(
+                ["docker", "push", f"{container_path}:{tag_version}"],
+                check=True,
+            )
 
 
 if __name__ == "__main__":
