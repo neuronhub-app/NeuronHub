ad55f337c9238767bba5ec7c4a573dcd8c69a6e2
Viktor <viktor+git@neuronhub.app>
2026-03-16 20:39:53 -0700

    UI: <LinkExt> add target="_blank" & style

--- docs/src/components/LinkExt.tsx
+++ docs/src/components/LinkExt.tsx
@@ -1,13 +1,19 @@
 import { Icon, Link } from "@chakra-ui/react";
 import { ReactNode } from "react";
-import { LuExternalLink } from "react-icons/lu";
+import { FiExternalLink } from "react-icons/fi";
 
 export function LinkExt(props: { href: string; children: ReactNode }) {
   return (
-    <Link href={props.href} target="_blank" rel="nofollow">
+    <Link
+      href={props.href}
+      target="_blank"
+      rel="nofollow"
+      display="inline-flex"
+      alignItems="center"
+    >
       {props.children}{" "}
-      <Icon boxSize="3.5" color="fg.muted">
-        <LuExternalLink />
+      <Icon boxSize="12px" color="fg.muted">
+        <FiExternalLink />
       </Icon>
     </Link>
   );
