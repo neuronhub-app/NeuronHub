5756a36b902dbe96390a97867149f9e67fb2affc
Viktor <viktor+git@neuronhub.app>
2026-03-16 20:34:11 -0700

    build(monitor): to Sentry add reactRouterV7 int in entry.client.tsx #draft

--- client/src/entry.client.tsx
+++ client/src/entry.client.tsx
@@ -1,16 +1,31 @@
-import * as Sentry from "@sentry/react-router";
-import { StrictMode, startTransition } from "react";
+import { StrictMode, startTransition, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
 import { env } from "@/env";
+import {
+  Routes,
+  createBrowserRouter,
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from "react-router";
+import * as Sentry from "@sentry/react";
 
 Sentry.init({
   enabled: env.isProd,
   dsn: env.VITE_SENTRY_DSN_FRONTEND,
   integrations: [
-    Sentry.reactRouterTracingIntegration(),
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
     Sentry.replayIntegration(),
     Sentry.feedbackIntegration({ colorScheme: "system" }),
+    Sentry.consoleLoggingIntegration({ levels: ["error", "warn"] }),
   ],
   tracesSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
@@ -20,9 +35,12 @@ Sentry.init({
   enableLogs: env.isProd,
   // setup API endpoint, see https://docs.sentry.io/platforms/javascript/guides/react-router/troubleshooting/#dealing-with-ad-blockers
   // tunnel: `${env.VITE_SERVER_URL_API}/error-handler`,
-  // todo ! IP collection is disabled, but need to add a good Scrubber.
+  // todo ! privacy: IP collection is disabled, but need to add a good Scrubber.
   sendDefaultPii: true,
 });
+
+const sentryCreateBrowserRouter = Sentry.wrapCreateBrowserRouterV7(createBrowserRouter);
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 startTransition(() => {
   hydrateRoot(
