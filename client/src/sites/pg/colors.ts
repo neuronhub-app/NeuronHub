34c92f7fd5d084eb8b1df7d3c39e86bc0ca8cfb9
Sofi Bel <91890402+sofi-bel@users.noreply.github.com>
2026-03-25 20:38:25 +0300

    fix(PG): pinned card slug URL match and 404
    Refs ENG-50

--- client/src/sites/pg/colors.ts
+++ client/src/sites/pg/colors.ts
@@ -32,16 +32,33 @@ export namespace theme {
     // todo !! fix: don't override the mandatory `bg.*` obj, or add all required `bg.*` keys
     // bg: { value: { _light: "{colors.brand.seashell}", _dark: "{colors.stone.950}" } },
 
+    bg: {
+      DEFAULT: { value: { _light: "{colors.brand.seashell}", _dark: "{colors.stone.950}" } },
+      subtle: { value: { _light: "{colors.gray.50}", _dark: "{colors.gray.950}" } },
+      muted: { value: { _light: "{colors.gray.100}", _dark: "{colors.gray.900}" } },
+      emphasized: { value: { _light: "{colors.gray.200}", _dark: "{colors.gray.800}" } },
+      inverted: { value: { _light: "{colors.black}", _dark: "{colors.white}" } },
+      panel: { value: { _light: "{colors.white}", _dark: "{colors.gray.950}" } },
+      error: { value: { _light: "{colors.red.50}", _dark: "{colors.red.950}" } },
+      warning: { value: { _light: "{colors.orange.50}", _dark: "{colors.orange.950}" } },
+      success: { value: { _light: "{colors.green.50}", _dark: "{colors.green.950}" } },
+      info: { value: { _light: "{colors.blue.50}", _dark: "{colors.blue.950}" } },
+      card: { value: { _light: "white", _dark: "{colors.slate.800}" } },
+      backdrop: { value: "{colors.brand.gray.backdrop}/50" },
+    },
+
     fg: {
       DEFAULT: { value: { _light: "{colors.brand.black}", _dark: "{colors.slate.200}" } },
       muted: { value: { _light: "{colors.brand.gray.muted}", _dark: "{colors.stone.400}" } },
       secondary: {
         value: { _light: "{colors.brand.black.secondary}", _dark: "{colors.slate.400}" },
       },
-      // todo !! fix: add required keys for `fg.*`
+      subtle: { value: { _light: "{colors.gray.400}", _dark: "{colors.gray.500}" } },
+      inverted: { value: { _light: "{colors.gray.50}", _dark: "{colors.black}" } },
+      error: { value: { _light: "{colors.red.500}", _dark: "{colors.red.400}" } },
+      warning: { value: { _light: "{colors.orange.600}", _dark: "{colors.orange.300}" } },
+      success: { value: { _light: "{colors.green.600}", _dark: "{colors.green.300}" } },
+      info: { value: { _light: "{colors.blue.600}", _dark: "{colors.blue.300}" } },
     },
-    "bg.card": { value: { _light: "white", _dark: "{colors.slate.800}" } },
-    "bg.inverted": { value: { _light: "{colors.black}", _dark: "{colors.white}" } },
-    "bg.backdrop": { value: "{colors.brand.gray.backdrop}/50" },
   };
 }
