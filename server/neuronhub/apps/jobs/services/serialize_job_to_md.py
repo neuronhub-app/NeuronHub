cd2e020b53241b940dcb5f4f2a3e48dfc8ebd0aa
Viktor <viktor+git@neuronhub.app>
2026-04-26 21:22:07 -0700

    refac(jobs/sync): MD format of serialize_job_to_md.py
    Refs: ENG-1

--- server/neuronhub/apps/jobs/services/serialize_job_to_md.py
+++ server/neuronhub/apps/jobs/services/serialize_job_to_md.py
@@ -4,20 +4,20 @@ from neuronhub.apps.jobs.models import Job
 from neuronhub.apps.jobs.models import JobLocation
 
 
-async def serialize_job_to_md(job: Job) -> str:
+async def serialize_job_to_md(job: Job, is_show_org_desc: bool | None = True) -> str:
     lines = [
-        f"## {job.title} | {job.org.name}",
-        f"- URL: {job.url_external}",
-        f"- Domain: {job.org.domain}",
-        f"- Website: {job.org.website}",
-        f"- Logo: {job.org.logo.name if job.org.logo else ''}",
+        f"**{job.title}** | {job.org.name}",
+        f"- `URL`: {job.url_external}",
+        f"- `Domain`: {job.org.domain}",
+        f"- `Website`: {job.org.website}",
+        f"- `Logo`: {job.org.logo.name if job.org.logo else ''}",
     ]
 
     if job.org.is_highlighted:
-        lines.append("- Org is highlighted")
+        lines.append("- `Org is highlighted`")
 
     if job.org.jobs_page_url:
-        lines.append(f"- Org jobs: {job.org.jobs_page_url}")
+        lines.append(f"- `Org jobs`: {job.org.jobs_page_url}")
 
     locations = [loc async for loc in job.locations.all()]
 
@@ -31,26 +31,28 @@ async def serialize_job_to_md(job: Job) -> str:
         )
     )
     if location_names_wo_dups:
-        lines.append(f"- Locations: {', '.join(location_names_wo_dups)}")
+        lines.append(f"- `Locations`: {', '.join(location_names_wo_dups)}")
 
     if job.posted_at:
-        lines.append(f"- Posted: {job.posted_at:%Y-%m-%d}")
+        lines.append(f"- `Posted`: {job.posted_at:%Y-%m-%d}")
     if job.closes_at:
-        lines.append(f"- Closes: {job.closes_at:%Y-%m-%d}")
+        lines.append(f"- `Closes`: {job.closes_at:%Y-%m-%d}")
 
     if job.salary_min:
-        lines.append(f"- Salary min: ${job.salary_min:,}")
+        lines.append(f"- `Salary min`: ${job.salary_min:,}")
     if job.salary_text:
-        lines.append(f"- Salary text: {job.salary_text}")
+        lines.append(f"- `Salary text`: {job.salary_text}")
 
     if job.source_ext:
-        lines.append(f"- Source: {job.source_ext}")
+        lines.append(f"- `Source`: {job.source_ext}")
 
     if job.is_duplicate_url_valid:
-        lines.append("- Duplicate URL: valid")
+        lines.append("- `Duplicate URL`: valid")
 
-    await _append_tags(lines, label="Area", manager=job.tags_area)
-    await _append_tags(lines, label="Skill", manager=job.tags_skill)
+    lines.append("")
+    lines.append("#### Tags")
+    await _append_tags(lines, label="Areas", manager=job.tags_area)
+    await _append_tags(lines, label="Skills", manager=job.tags_skill)
     await _append_tags(lines, label="Education", manager=job.tags_education)
     await _append_tags(lines, label="Experience", manager=job.tags_experience)
     await _append_tags(lines, label="Workload", manager=job.tags_workload)
@@ -60,7 +62,7 @@ async def serialize_job_to_md(job: Job) -> str:
         lines.append("### Description")
         lines.append(job.description)
 
-    if job.org.description:
+    if job.org.description and is_show_org_desc:
         lines.append("")
         lines.append("### Org")
         lines.append(job.org.description)
@@ -72,4 +74,4 @@ async def _append_tags(lines: list[str], *, label: str, manager: Manager) -> Non
     # `.all()` for prefetch cache
     names = sorted([tag.name async for tag in manager.all()])
     if names:
-        lines.append(f"- {label}: {', '.join(names)}")
+        lines.append(f"- `{label}`: {', '.join(names)}")
