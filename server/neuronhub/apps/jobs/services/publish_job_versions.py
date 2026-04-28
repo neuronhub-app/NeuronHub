8ec909d769751a11df8328fe8b1d4ce4ce915741
Viktor <viktor+git@neuronhub.app>
2026-04-27 20:55:43 -0700

    fix(jobs/sync): use custom_objectID="slug" in publish_job_versions.py
    Refs: ENG-1, #168

--- server/neuronhub/apps/jobs/services/publish_job_versions.py
+++ server/neuronhub/apps/jobs/services/publish_job_versions.py
@@ -15,46 +15,50 @@ from neuronhub.apps.jobs.services.airtable_sync_jobs import get_jobs_qs_prefetch
 
 
 @dataclass
-class JobIds:
-    created: list[int] = field(default_factory=list)
-    updated: list[int] = field(default_factory=list)
-    deleted: list[int] = field(default_factory=list)
+class JobSlugs:
+    created: list[SlugStr] = field(default_factory=list)
+    updated: list[SlugStr] = field(default_factory=list)
+    deleted: list[SlugStr] = field(default_factory=list)
 
 
-async def publish_job_versions(draft_ids: list[int]):
+type SlugStr = str
+type ID = int
+
+
+async def publish_job_versions(draft_ids: list[ID]):
     sentry_sdk.set_context("draft_ids", dict(ids=draft_ids))
 
     with disable_auto_indexing_if_enabled():
-        job_ids = await _publish_changes_to_db(draft_ids)
+        job_slugs = await _publish_changes_to_db(draft_ids)
 
-    sentry_sdk.set_context("job_ids", asdict(job_ids))
+    sentry_sdk.set_context("job_slugs", asdict(job_slugs))
 
     if settings.ALGOLIA["IS_ENABLED"]:
-        await _publish_changes_to_algolia(job_ids)
+        await _publish_changes_to_algolia(job_slugs)
 
 
-async def _publish_changes_to_db(draft_ids: list[int]) -> JobIds:
-    job_ids = JobIds()
+async def _publish_changes_to_db(draft_ids: list[ID]) -> JobSlugs:
+    job_slugs = JobSlugs()
 
     result_del = await _publish_deletion(draft_ids)
-    job_ids.deleted += result_del.deleted
+    job_slugs.deleted += result_del.deleted
 
     result_new = await _publish_update_or_create(draft_ids)
-    job_ids.updated += result_new.updated
-    job_ids.created += result_new.created
+    job_slugs.updated += result_new.updated
+    job_slugs.created += result_new.created
 
     await _handle_invalid_drafts(draft_ids)
 
-    return job_ids
+    return job_slugs
 
 
-async def _publish_deletion(draft_ids: list[int]) -> JobIds:
-    job_ids = JobIds()
+async def _publish_deletion(draft_ids: list[ID]) -> JobSlugs:
+    job_slugs = JobSlugs()
 
     jobs_draft = Job.objects.filter(id__in=draft_ids, is_pending_removal=True)
     jobs_pub = Job.objects.filter(versions__in=jobs_draft, is_published=True).distinct()
 
-    job_ids.deleted = [id async for id in jobs_pub.values_list("id", flat=True)]
+    job_slugs.deleted = [slug async for slug in jobs_pub.values_list("slug", flat=True)]
 
     async for job_pub in jobs_pub:  # for django-simple-history, JIC
         try:
@@ -67,11 +71,11 @@ async def _publish_deletion(draft_ids: list[int]) -> JobIds:
     except Exception:
         sentry_sdk.capture_exception()
 
-    return job_ids
+    return job_slugs
 
 
-async def _publish_update_or_create(draft_ids: list[int]) -> JobIds:
-    job_ids = JobIds()
+async def _publish_update_or_create(draft_ids: list[ID]) -> JobSlugs:
+    job_slugs = JobSlugs()
 
     async for job_updated_draft in Job.objects.filter(
         id__in=draft_ids,
@@ -86,7 +90,7 @@ async def _publish_update_or_create(draft_ids: list[int]) -> JobIds:
             job_updated_draft.is_published = True
             await job_updated_draft.asave()
 
-            job_ids.updated.append(job_updated_draft.id)
+            job_slugs.updated.append(job_updated_draft.slug)
         except Exception:
             sentry_sdk.capture_exception()
 
@@ -97,16 +101,18 @@ async def _publish_update_or_create(draft_ids: list[int]) -> JobIds:
         version_of__isnull=True,
     )
     try:
-        job_ids.created = [id async for id in drafts_to_create.values_list("id", flat=True)]
+        job_slugs.created = [
+            slug async for slug in drafts_to_create.values_list("slug", flat=True)
+        ]
         await drafts_to_create.aupdate(is_published=True)
     except Exception:
         sentry_sdk.capture_exception()
 
-    return job_ids
+    return job_slugs
 
 
 @sync_to_async
-def _publish_changes_to_algolia(job_ids: JobIds):
+def _publish_changes_to_algolia(job_slugs: JobSlugs):
     from algoliasearch_django import algolia_engine
 
     adapter = algolia_engine.get_adapter(model=Job)
@@ -114,16 +120,16 @@ def _publish_changes_to_algolia(job_ids: JobIds):
 
     algolia_engine.client.delete_objects(
         index_name=adapter.index_name,
-        object_ids=job_ids.deleted,
+        object_ids=job_slugs.deleted,
         wait_for_tasks=is_await_algolia_http_response,
     )
 
     for algolia_params in [
-        AlgoliaParams(ids=job_ids.updated, method="partial_update_objects"),
-        AlgoliaParams(ids=job_ids.created, method="save_objects"),
+        AlgoliaParams(slugs=job_slugs.updated, method="partial_update_objects"),
+        AlgoliaParams(slugs=job_slugs.created, method="save_objects"),
     ]:
         jobs_changed: list[dict] = []
-        for job in get_jobs_qs_prefetched().filter(id__in=algolia_params.ids):
+        for job in get_jobs_qs_prefetched().filter(slug__in=algolia_params.slugs):
             if adapter._should_index(job):
                 jobs_changed.append(adapter.get_raw_record(job))
 
@@ -137,10 +143,10 @@ def _publish_changes_to_algolia(job_ids: JobIds):
 @dataclass
 class AlgoliaParams:
     method: Literal["partial_update_objects", "save_objects"]
-    ids: list[int]
+    slugs: list[SlugStr]
 
 
-async def _handle_invalid_drafts(draft_ids: list[int]):
+async def _handle_invalid_drafts(draft_ids: list[ID]):
     """
     #AI
 
