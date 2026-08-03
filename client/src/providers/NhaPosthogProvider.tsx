837a3118854d19db7e4bb2c21e0ce90639eab6a5
Viktor <viktor+git@neuronhub.app>
2026-08-03 14:03:02 -0700

    fix(track): never send private props (as GeoIP)

--- client/src/providers/NhaPosthogProvider.tsx
+++ client/src/providers/NhaPosthogProvider.tsx
@@ -2,7 +2,7 @@
  * todo ? refac: don't have a folder with a single Comp. Move it up, or place more Providers in this dir.
  */
 import { PostHogProvider } from "@posthog/react";
-import posthog from "posthog-js";
+import posthog, { CaptureResult } from "posthog-js";
 import { ReactNode } from "react";
 
 import { env } from "@/env";
@@ -19,6 +19,21 @@ export function NhaPosthogProvider(props: { children: ReactNode }) {
       },
       defaults: "2026-01-30",
       internal_or_test_user_hostname: "pg.stage.neuronhub.app|localhost",
+      before_send: function (event) {
+        for (const propPrivate of [
+          "$ip",
+          "$raw_user_agent",
+          "$geoip_latitude",
+          "$geoip_longitude",
+          "$geoip_accuracy_radius",
+          "$geoip_postal_code",
+        ]) {
+          if (event?.properties[propPrivate]) {
+            event.properties[propPrivate] = null;
+          }
+        }
+        return event;
+      },
     });
     return <PostHogProvider client={posthog}>{props.children}</PostHogProvider>;
   }
